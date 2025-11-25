eefed046107916544e495dcfe76c05b64af1e1f9
Raj-P08 <rajkumarpatil775694@gmail.com>
2025-11-25 18:05:45 +0530

    resolved prompt is not defined problem

--- Array/script.js
+++ Array/script.js
@@ -7,10 +7,23 @@
 // Q . 2 Check Value Exists
 // Check if a given value exists in an array of numbers.
 
-let arr = [4,5,19,6];
-const value = Number(prompt("Enter the Num:" ));
-if (arr.includes(value)) {
-    console.log("num exists");
+// const prompt = require("prompt-sync")(); // sirf Node.js
+const ar = [8,9,4,45,69];
+const value = Number(prompt("Enter the Random Number: "));
+
+if(ar.includes(value)){
+    console.log("number exists");
 }else{
-    console.log("not exists");
+    console.log("number not exists");
 }
+
+// Here we are facing one prblem that is prompt is not defined. 
+// It is showing cause prompt is working only in browser window.
+//  We cannot use the prompt in vs code or terminal execution.
+//  to resolve this we can use the HTML file and run the file then the prompt will display
+//  to use the prompt in vs code there are 2 ways.
+// Option A : Use (prompt-sync) package
+// const prompt = require("prompt-sync")(); // sirf Node.js
+//  for the above we have to install package i.e. npm install prompt-sync
+
+// Option B : we can use the manual value for the variable means hard code
